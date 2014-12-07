1ba51cda0b0f8d7b24c1741fc00b090269e8a1e4
Niraj Pant <nirajpant7@gmail.com>
2014-12-07 16:33:08 -0600

    add images + obj-c headers

---
+++ BitWatch/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  Tracker.swift
+//  BitWatch
+//
+//  Created by Niraj Pant on 12/7/2014.
+//  Copyright (c) 2014 Niraj Pant All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  
+  var window: UIWindow?
+  
+  func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+    return true
+  }
+  
+}
+
---
+++ BitWatchKit/BitWatchKit.h
@@ -0,0 +1,19 @@
+//
+//  Tracker.swift
+//  BitWatch
+//
+//  Created by Niraj Pant on 12/7/2014.
+//  Copyright (c) 2014 Niraj Pant All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BitWatchKit.
+FOUNDATION_EXPORT double BitWatchKitVersionNumber;
+
+//! Project version string for BitWatchKit.
+FOUNDATION_EXPORT const unsigned char BitWatchKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BitWatchKit/PublicHeader.h>
+
+
